2b2c3760c491a9e26a1c461cbce9d79da18c4e08
unknown <raiyan24r@gmail.com>
2020-05-28 22:03:19 +0600

    TravelV1

---
+++ travel_app/lib/screens/Content/DivisionPanels/SlidingPanelDivisions.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'SylhetPanel.dart';
+import 'ChittagongPanel.dart';
+import 'BarishalPanel.dart';
+import 'KhulnaPanel.dart';
+import 'RajshahiPanel.dart';
+import 'RangpurPanel.dart';
+import 'MymensinghPanel.dart';
+import 'DhakaPanel.dart';
+
+
+List<Widget> divListPanel = [
+  PanelSylhet(),
+  PanelDhaka(),
+  PanelChittagong(),
+  PanelBarishal(),
+  PanelKhulna(),
+  PanelRajshahi(),
+  PanelRangpur(),
+  PanelMymensingh(),
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
