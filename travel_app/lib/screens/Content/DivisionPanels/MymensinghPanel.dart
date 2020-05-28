b491bf6bdb4281fe338b4456350607ef37b8af9f
raiyan24r <raiyan24r@gmail.com>
2020-05-28 22:33:20 +0600

    Update MymensinghPanel.dart
    added comments

--- travel_app/lib/screens/Content/DivisionPanels/MymensinghPanel.dart
+++ travel_app/lib/screens/Content/DivisionPanels/MymensinghPanel.dart
@@ -3,15 +3,17 @@ import 'package:flutter_svg/flutter_svg.dart';
 import '../ClassesData.dart';
 import '../CustomDistrictButton.dart';
 
-
-
 class PanelMymensingh extends StatefulWidget {
   PanelMymensingh({Key key}) : super(key: key);
 
   @override
   _PanelMymensinghState createState() => _PanelMymensinghState();
 }
+
 var mymensinghcity = new Districts("mymensingh", false);
+// TODO: Add class for each district here
+//      example : var *classname* = new Districts("*districtname", false);
+
 class _PanelMymensinghState extends State<PanelMymensingh> {
   @override
   Widget build(BuildContext context) {
@@ -23,6 +25,8 @@ class _PanelMymensinghState extends State<PanelMymensingh> {
             'assets/images/mymensingh.svg',
           ),
           DistrictButton('MYMENSINGH', mymensinghcity, null, null, 110, 200),
+          // TODO: Add buttons 
+          //DistrictButton('*jei text boshbe', upore classname jeita disi, left theke distance, top, right, bottom),
         ],
       )),
     );
